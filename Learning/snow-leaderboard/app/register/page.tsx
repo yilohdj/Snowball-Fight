8025e0bdebd5757c969b12ab885b5c73b8691747
yilohdj <yilohdj@gmail.com>
2025-03-13 01:28:28 -0400

    more uneccessary code

--- Learning/snow-leaderboard/app/register/page.tsx
+++ Learning/snow-leaderboard/app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import React, { useState} from "react"
 import { useRouter } from "next/navigation"
 import { ArrowLeft, Loader2 } from 'lucide-react'
 import Link from "next/link"
